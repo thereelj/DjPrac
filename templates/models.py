00cec91197333cedec9dabb115691fb242c54e32
Unknown <jezreelvyanez@gmail.com>
2018-04-25 22:41:24 +0800

    Initial commit
    First Commit

---
+++ templates/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.utils import timezone
+class Comment(models.Model):
+	name = models.CharField(max_length=20)
+	comment = models.TextField()
+	date_added = models.DateTimeField(default=timezon.now)
